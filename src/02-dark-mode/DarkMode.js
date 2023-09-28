a3ad6b17143b471f3f5bb489521f4c99aea7e707
amy-siton <amysiton@users.noreply.github.com>
2023-09-29 00:36:28 +0800

    Update DarkMode.js

--- src/02-dark-mode/DarkMode.js
+++ src/02-dark-mode/DarkMode.js
@@ -1,8 +1,13 @@
+import { useEffect, useState } from "react"
+
 export default function DarkMode () {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <div className='page'>
-      <button className='dark-mode-button'>Dark Mode</button>
-      <button className='light-mode-button'>Light Mode</button>
+    <div className={`${darkMode ? "dark-mode" : ""} page`}>
+      <button 
+        className='dark-mode-button' onClick={() => setDarkMode(true)}>Dark Mode</button>
+      <button className='light-mode-button' onClick={() => setDarkMode(false)}>Light Mode</button>
     </div>
   )
 }

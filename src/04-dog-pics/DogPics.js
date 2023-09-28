93055b811881af21b2b7a2004e1258a7a3aa426d
amy-siton <amysiton@users.noreply.github.com>
2023-09-29 02:45:13 +0800

    Update DogPics.js

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
+
 export default function DogPics () {
-  // API: https://dog.ceo/dog-api/
+  const [imgSrc, setImgSrc] = useState('');
+  const apiUrl = "https://dog.ceo/api/breeds/image/random";
+
+  const fetchDog = async() => {
+    await fetch(apiUrl)
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setImgSrc(data.message)
+      });
+  }
+
+  useEffect(() => {
+    fetchDog();
+  }, [])
+
   return (
     <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
+      <img src={`${imgSrc}`} alt="" width="auto" height="400px"/>
+      <button onClick={() => fetchDog()}>🐶</button>
     </div>
   )
 }

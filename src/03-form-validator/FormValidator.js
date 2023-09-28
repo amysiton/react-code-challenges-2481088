96eceb5855d1f6945f1b3878e2cdd0cfb07a1ba4
amy-siton <amysiton@users.noreply.github.com>
2023-09-29 02:22:41 +0800

    Update FormValidator.js

--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -4,6 +4,26 @@ export default function FormValidator () {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [message, setMessage] = useState('');
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    const validateEmail = email.match(/^\S+@\S+\.\S+$/);
+    let validatePassword = false;
+
+    console.log(validateEmail);
+
+    if (password === passwordConfirm) {
+      validatePassword = password.length > 8;
+    }
+
+    if (validateEmail && validatePassword) {
+      setMessage("User created");
+    } else {
+      setMessage("An email must have exactly one @ sign, passwords must be 8 characters or longer");
+    }
+  }
 
   return (
     <form>
@@ -12,18 +32,24 @@ export default function FormValidator () {
       <input
         type='text' name='email'
         onChange={e => setEmail(e.target.value)}
+        required={true}
       />
       <label htmlFor='password'>Password</label>
       <input
         type='password' name='password'
         onChange={e => setPassword(e.target.value)}
+        required={true}
       />
       <label htmlFor='password-confirm'>Confirm Password </label>
       <input
         type='password' name='password-confirm'
         onChange={e => setPasswordConfirm(e.target.value)}
+        required={true}
       />
-      <input type='submit' value='Submit' />
+      <p>{message}</p>
+      <input type='submit' value='Submit'
+        onClick={ e => submitForm(e)}
+      />
     </form>
   )
 }

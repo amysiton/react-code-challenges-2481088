30ce7355290c882a1869ebacc3670657e0106e82
amysiton <amysiton@users.noreply.github.com>
2023-10-12 21:19:32 +0800

    Update SimpleCalculator

--- src/09-simple-calculator/SimpleCalculator.js
+++ src/09-simple-calculator/SimpleCalculator.js
@@ -1,33 +1,69 @@
-const initialState = {}
+import { useReducer } from "react";
 
-function reducer (state, action) {}
+const initialState = {
+  number1: 0,
+  number2: 0,
+  result: "No result yet"
+};
+
+function reducer (state, action) {
+  const { type } = action;
+
+  switch(type) {
+    case "SET_NUMBER1": {
+      return { ...state, number1: action.number }
+    }
+    case "SET_NUMBER2": {
+      return { ...state, number2: action.number }
+    }
+    case "ADD": {
+      return { ...state, result: state.number1 + state.number2 }
+    }
+    case "SUBTRACT": {
+      return { ...state, result: state.number1 - state.number2 }
+    }
+    case "CLEAR": {
+      return initialState
+    }
+    default: {
+      throw new Error();
+    }
+  }
+}
 
 export default function SimpleCalculator () {
-  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div>
       <div>
         <h2>Number 1</h2>
         {numbers.map(number => (
-          <button key={number}>
+          <button 
+            key={number}
+            onClick={() => dispatch({ type: "SET_NUMBER1", number })}
+          >
             {number}
           </button>))}
       </div>
       <div>
         <h2>Number 2</h2>
         {numbers.map(number => (
-          <button key={number}>
+          <button 
+            key={number}
+            onClick={() => dispatch({ type: "SET_NUMBER2", number })}
+          >
             {number}
           </button>))}
       </div>
       <div>
         <h2>Actions</h2>
-        <button>+</button>
-        <button>-</button>
-        <button>c</button>
+        <button onClick={() => dispatch({type: "ADD"})}>+</button>
+        <button onClick={() => dispatch({type: "SUBTRACT"})}>-</button>
+        <button onClick={() => dispatch({type: "CLEAR"})}>c</button>
       </div>
-      <div><h2>Result:</h2></div>
+      <div><h2>Result: {state.result}</h2></div>
     </div>
   )
 }
